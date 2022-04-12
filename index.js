504d21c532c9306f77df1dc9d8b4fc4672eca9b2
trisdauvergne <tristan.dauvergne@gmail.com>
2022-04-12 15:48:57 +0200

    Early stage of sockets in index.js but figuring out the broadcast emit feature

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import listsRoutes from './routes/lists.js';
 import reminderRoutes from './routes/reminders.js'
 import { connectDB } from './config/db.js';
 
+// socket io
+import * as http from 'http'; // socket io
+import { Server } from 'socket.io'; // socket io
+
 dotenv.config();
 
 const app = express();
@@ -18,6 +22,29 @@ app.use(express.json());
 app.use('/lists', listsRoutes);
 app.use('/reminders', reminderRoutes);
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+// socket io
+const server = http.createServer(app); // socket io
+
+export const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    },
+}); // socket io
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+}); // socket io
+
+io.on('connection', (socket) => {
+    console.log(`Socket ID: ${socket.id}`);
+
+    socket.on('send_reminder', (data) => {
+        console.log('send message', data);
+        socket.broadcast.emit('receive_message', data);
+    })
+}) // socket io
+
+// app.listen(port, () => {
+//     console.log(`Listening on port ${port}`);
+// })
